4b306d974b44f7e8444814ecc8306c1cd15f8c93
koudaihirata <2230051@ecc.ac.jp>
2024-08-13 00:21:31 +0900

    仮データ挿入

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PokemonThumbnails from './PokemonThumbnails';
 
 function App() {
+  const pokemon = {
+    id: 1,
+    name: "フシギダネ",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    type: "くさ"
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PokemonThumbnails id={pokemon.id} name={pokemon.name} image={pokemon.image} type={pokemon.type} />
     </div>
   );
 }
---
+++ src/PokemonThumbnails.tsx
@@ -0,0 +1,22 @@
+
+type poke = {
+    id: number,
+    name: string,
+    image: string,
+    type: string
+}
+
+export default function PokemonThumbnails({ id, name, image, type }:poke) {
+    return(
+        <div>
+            <div className="number">
+                <small>#0{id}</small>
+            </div>
+            <img src={image} alt={name} />
+            <div className="detail-wrapper">
+                <h4>{name}</h4>
+                <h3>{type}</h3>
+            </div>
+        </div>
+    )
+}
