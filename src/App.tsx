ef9e54019e861329d8bcf6134634c84762c4f296
koudaihirata <2230051@ecc.ac.jp>
2024-08-13 00:54:46 +0900

    ポケモンをAPIから表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
 import PokemonThumbnails from './PokemonThumbnails';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+type Pokemon = {
+  id: number;
+  name: string;
+  image: string;
+  type: string;
+};
 
 function App() {
-  const pokemons = [
-    {
-      id: 1,
-      name: "フシギダネ",
-      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      type: "くさ"
-    },
-    {
-      id: 2,
-      name: "フシギソウ",
-      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
-      type: "くさ"
-    },
-    {
-      id: 3,
-      name: "フシギバナ",
-      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
-      type: "くさ"
-    },
-  ]
+  const [allPokemons, setAllPokemons] = useState<Pokemon[]>([]);
 
-  const url = "https://pokeapi.co/api/v2/pokemon";
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon?limit=20");
 
   useEffect(() => {
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-      })
-  },[])
+    const getAllPokemons = () => {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => {
+          setUrl(data.next);
+
+          // 各ポケモンの詳細データを取得して更新
+          const results = data.results;
+          for (const pokemon of results) {
+            const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`;
+            fetch(pokemonUrl)
+              .then(res => res.json())
+              .then(data => {
+                const _image = data.sprites.other["official-artwork"].front_default;
+                const _type = data.types[0].type.name;
+                const newList: Pokemon = {
+                  id: data.id,
+                  name: data.name,
+                  image: _image,
+                  type: _type
+                };
+                // 既存のデータを展開し、新しいデータを追加する
+                setAllPokemons(currentList => [...currentList, newList]);
+              });
+          }
+        });
+    };
+
+    getAllPokemons();
+  }, [url]);
 
   return (
     <div className="App">
-      {pokemons.map((pokemon, index) => (
+      {allPokemons.map((pokemon) => (
         <PokemonThumbnails 
           id={pokemon.id} 
           name={pokemon.name} 
